3866be03629bdf83f5d47ec43cc174698442b9a0
ArdianR <fianr5750@gmail.com>
2018-02-28 18:53:24 +0700

    penambahan database asli dari laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,110 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('areas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('alias');
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('desc');
+            $table->string('spec');
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('DealerID');
+            $table->string('name');
+            $table->string('address');
+            $table->integer('area_id')->unsigned();
+            $table->foreign('area_id')->references('id')->on('areas');
+            $table->string('grade');
+            $table->integer('status');
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->integer('area_id')->unsigned();
+            $table->foreign('area_id')->references('id')->on('areas');
+            $table->string('email');
+            $table->string('contact');
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('group')->unsigned();
+            $table->foreign('group')->references('id')->on('groups');
+            $table->integer('area')->unsigned();
+            $table->foreign('area')->references('id')->on('areas');
+            $table->tinyInteger('active')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('pops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('area_id')->unsigned();
+            $table->foreign('area_id')->references('id')->on('areas');
+            $table->integer('store_id')->unsigned();
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('photo');
+            $table->tinyInteger('posisi')->default(0);
+            $table->tinyInteger('ukuran')->default(0);
+            $table->text('note');
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('active')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('pop_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pop_id')->unsigned();
+            $table->foreign('pop_id')->references('id')->on('pops');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('job_title');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -31,5 +131,15 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('groups');
+        Schema::dropIfExists('areas');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('stores');
+        Schema::dropIfExists('vendors');
+        Schema::dropIfExists('pops');
+        Schema::dropIfExists('pop_details');
+        Schema::dropIfExists('warehouses');
+        Schema::dropIfExists('admins');
+
     }
 }
